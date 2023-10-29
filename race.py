817da68deec474af163d850705de10b7f1de56c1
Anderis <88600844+Anderis@users.noreply.github.com>
2023-10-29 13:20:34 -0500

    Race Data

--- race.py
+++ race.py
@@ -11,3 +11,72 @@ race_aliases = {
     'Human': ['Man', 'Hu', 'Hum', 'HMan'],
     'Tiefling': ['Tief','T', 'TLing']  
 }
+
+race_data = {
+    'Dragonborn': {
+        'title': 'Dragonborn',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Dragonborn.png',
+        'color': 0xF77709 #Placeholder     
+        },
+    'Drow': {
+        'title': 'Drow',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Drow.png',
+        'color': 0xF77709 #Placeholder     
+        },
+    'Dwarf': {
+        'title': 'Dwarf',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Dwarf.png',
+        'color': 0xF77709 #Placeholder     
+        },
+    'Elf': {
+        'title': 'Elf',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Elf.png',
+        'color': 0xF77709 #Placeholder     
+        },
+    'Githyanki': {
+        'title': 'Githyanki',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Githyanki.png',
+        'color': 0xF77709 #Placeholder     
+        },
+    'Gnome': {
+        'title': 'Gnome',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Gnome.png',
+        'color': 0xF77709 #Placeholder     
+        },
+    'Half-Elf': {
+        'title': 'Half-Elf',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Half-Elf.png',
+        'color': 0xF77709 #Placeholder     
+        },
+    'Half-Orc': {
+        'title': 'Half-Orc',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Half-Orc.png',
+        'color': 0xF77709 #Placeholder     
+        },
+    'Halfling': {
+        'title': 'Halfling',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Halfling.png',
+        'color': 0xF77709 #Placeholder     
+        },
+    'Human': {
+        'title': 'Human',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Human.png',
+        'color': 0xF77709 #Placeholder     
+        },
+    'Tiefling': {
+        'title': 'Tiefling',
+        'description': '',
+        'image_url': 'https://raw.githubusercontent.com/Anderis/WithersBot/main/images/races/Tiefling.png',
+        'color': 0xF77709 #Placeholder     
+        },
+}
