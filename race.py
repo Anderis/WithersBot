6717dbfc1cbc46809422c9dbed1b159a06cdeebf
Anderis <88600844+Anderis@users.noreply.github.com>
2023-10-29 13:06:31 -0500

    Aliases

---
+++ race.py
@@ -0,0 +1,13 @@
+hero_aliases = {
+    'Dragonborn': ['DB', 'DragonB', 'DBorn', 'Dragon'],
+    'Drow': ['Dr','Dro'],
+    'Dwarf': ['Dw', 'Dwa', 'Dwar'],
+    'Elf': ['E', 'El'],
+    'Githyanki': ['Gi', 'Gith', 'Yanki', 'Githy', 'Githyank', 'Gi'],
+    'Gnome': ['Gn', 'Gno', 'Gnom'],
+    'Half-Elf': ['H-Elf', 'HElf', 'Half-E', 'HalfE', 'HE', 'H-E'],
+    'Half-Orc': ['H-Orc','HOrc', 'Half-O','HalfO', 'HO', 'H-O'],
+    'Halfling': ['Half',  'Hling', 'HalfL'],
+    'Human': ['Man', 'Hu', 'Hum', 'HMan'],
+    'Tiefling': ['Tief','T', 'TLing']  
+}
